502c4f85a389e4d499d5e453f4b04856b06bfa37
tahid02 <tanzim1463@gmail.com>
2022-06-05 03:41:32 +0600

    show the bookmarked time in popup

--- popup.js
+++ popup.js
@@ -1,14 +1,53 @@
 // adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+const addNewBookmark = (bookmarks, bookmark) => {
+  const bookmarkTitleElement = document.createElement("div");
+  const controlsElement = document.createElement("div");
+  const newBookmarkElement = document.createElement("div");
 
-const viewBookmarks = () => {};
+  bookmarkTitleElement.textContent = bookmark.desc;
+  bookmarkTitleElement.className = "bookmark-title";
+  controlsElement.className = "bookmark-controls";
+
+  setBookmarkAttributes("play", onPlay, controlsElement);
+  setBookmarkAttributes("delete", onDelete, controlsElement);
+
+  newBookmarkElement.id = "bookmark-" + bookmark.time;
+  newBookmarkElement.className = "bookmark";
+  newBookmarkElement.setAttribute("timestamp", bookmark.time);
+
+  newBookmarkElement.appendChild(bookmarkTitleElement);
+  newBookmarkElement.appendChild(controlsElement);
+  bookmarks.appendChild(newBookmarkElement);
+};
+
+const viewBookmarks = (currentBookmarks = []) => {
+  const bookmarksElement = document.getElementById("bookmarks");
+  bookmarksElement.innerHTML = "";
+
+  if (currentBookmarks.length > 0) {
+    for (let i = 0; i < currentBookmarks.length; i++) {
+      const bookmark = currentBookmarks[i];
+      addNewBookmark(bookmarksElement, bookmark);
+    }
+  } else {
+    bookmarksElement.innerHTML = '<i class="row">No bookmarks to show</i>';
+  }
+
+  return;
+};
 
 const onPlay = (e) => {};
 
 const onDelete = (e) => {};
 
-const setBookmarkAttributes = () => {};
+const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
+  const controlElement = document.createElement("img");
 
+  controlElement.src = "assets/" + src + ".png";
+  controlElement.title = src;
+  controlElement.addEventListener("click", eventListener);
+  controlParentElement.appendChild(controlElement);
+};
 // get the current video bookmark (if available) from local storage
 document.addEventListener("DOMContentLoaded", async () => {
   const activeTab = await getActiveTabURL();
@@ -22,6 +61,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       const currentVideoBookmarks = data[currentVideo]
         ? JSON.parse(data[currentVideo])
         : [];
+      viewBookmarks(currentVideoBookmarks);
     });
   } else {
     const container = document.getElementsByClassName("container")[0];
