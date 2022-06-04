68c637043301edadf45abd7031c3ea00fe54a28f
tahid02 <tanzim1463@gmail.com>
2022-06-05 02:30:01 +0600

    get video bookmark from storage

--- popup.js
+++ popup.js
@@ -3,10 +3,30 @@ const addNewBookmark = () => {};
 
 const viewBookmarks = () => {};
 
-const onPlay = e => {};
+const onPlay = (e) => {};
 
-const onDelete = e => {};
+const onDelete = (e) => {};
 
-const setBookmarkAttributes =  () => {};
+const setBookmarkAttributes = () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+// get the current video bookmark (if available) from local storage
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = await getActiveTabURL();
+  const queryParameters = activeTab.url.split("?")[1];
+  const urlParameters = new URLSearchParams(queryParameters);
+
+  const currentVideo = urlParameters.get("v");
+
+  if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+    chrome.storage.sync.get([currentVideo], (data) => {
+      const currentVideoBookmarks = data[currentVideo]
+        ? JSON.parse(data[currentVideo])
+        : [];
+    });
+  } else {
+    const container = document.getElementsByClassName("container")[0];
+
+    container.innerHTML =
+      '<div class="title">This is not a youtube video page.</div>';
+  }
+});
